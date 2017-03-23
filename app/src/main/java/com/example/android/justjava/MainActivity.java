0192d8daa79efea8dd3d70ab666d4643b981958f
Razvan Andrei Ionescu <ionescu77@gmail.com>
2017-03-23 14:47:57 +0200

    Some fixes :-p

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This is GLOBAL Variable declaration.
      */
-    int quantity = 2;
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
-        int quantity = 2;
         quantity = quantity + 1;
         display(quantity);
     }
@@ -60,7 +59,8 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
